b4877b588e1eedb45cd71186396512b5bad331c8
Rob Attfield <emailme@robertattfield.com>
2017-12-01 00:29:55 +1300

    Created comments table migration.

---
+++ database/migrations/2017_11_30_111838_create_comments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->text('comment');
+            $table->unsignedInteger('film_id');
+            $table->foreign('film_id')
+                ->references('id')
+                ->on('films')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign(['film_id']);
+        });
+        Schema::dropIfExists('comments');
+        Schema::enableForeignKeyConstraints();
+    }
+}
